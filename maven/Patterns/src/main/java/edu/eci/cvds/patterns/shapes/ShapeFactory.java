09e0ff0a1abf8d91f2ee53bbd826e073d3d108a8
JuanManuelHerreraMoya <57697133+JuanManuelHerreraMoya@users.noreply.github.com>
2020-01-28 22:50:56 -0500

    revisar ejec

--- maven/Patterns/src/main/java/edu/eci/cvds/patterns/shapes/ShapeFactory.java
+++ maven/Patterns/src/main/java/edu/eci/cvds/patterns/shapes/ShapeFactory.java
@@ -1,5 +1,25 @@
 package edu.eci.cvds.patterns.shapes;
+import edu.eci.cvds.patterns.shapes.concrete.*;
 
-public class ShapeFactory(){
+public class ShapeFactory{
 
+
+	public static Shape create (RegularShapeType type){
+		Shape fig = null;
+		switch(type){
+			case Triangle :
+				fig = new Triangle();
+				break;
+			case Hexagon : 
+				fig= new Hexagon();
+				break;
+			case Pentagon: 
+				fig = new Pentagon();
+				break;
+			case Quadrilateral: 
+				fig = new Quadrilateral();
+				break;			
+		}
+		return fig;
+	}
 }
